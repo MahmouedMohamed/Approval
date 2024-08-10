210767eb0cc8d924ed836fd09f6c73cf9e2437e6
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-10 22:54:42 +0300

    Add Approval Service Provider to Enable Migration Migrating

---
+++ src/ApprovalServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mahmoued\Approval;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApprovalServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot up Approval.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerMigrations();
+    }
+
+    /**
+     * Register Approval migrations.
+     *
+     * @return void
+     */
+    private function registerMigrations()
+    {
+        $this->publishes([
+            __DIR__ . '/Migrations' => database_path('migrations'),
+        ], 'migrations');
+    }
+}
